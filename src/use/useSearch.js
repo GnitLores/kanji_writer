243ef0c83a19b84ee1b5a31355125ad7ea94db8c
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-02-05 11:06:55 +0100

    parse search input changes

--- src/use/useSearch.js
+++ src/use/useSearch.js
@@ -12,7 +12,24 @@ export function useSearch() {
   };
 
   const onInputChange = (newInput, oldInput) => {
-    console.log(oldInput, newInput);
+    if (newInput === oldInput) return;
+    if (oldInput === "" && newInput !== "") startSearch();
+    if (newInput === "" && oldInput !== "") stopSearch();
+
+    if (newInput.length > oldInput.length)
+      addChars(newInput.slice(oldInput.length));
+
+    if (newInput.length < oldInput.length)
+      removeChars(oldInput.slice(newInput.length));
+  };
+
+  const startSearch = () => {};
+  const stopSearch = () => {};
+  const addChars = (chars) => {
+    console.log("add", chars);
+  };
+  const removeChars = (chars) => {
+    console.log("remove", chars);
   };
 
   return { input, clearSearch, onInputChange };
