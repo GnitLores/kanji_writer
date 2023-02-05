acea6f5f77eb8c50c806e6ee45bf8c1b532295d3
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-02-05 10:09:49 +0100

    search field icon

--- src/components/kanjiGridComponents/AppKanjiGridSearch.vue
+++ src/components/kanjiGridComponents/AppKanjiGridSearch.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="flex justify-center">
-    <input
-      type="text"
-      v-model="input"
-      placeholder="Enter text with kanji..."
-      class="w-96 bg-darkmode-100 border border-black rounded"
-    />
+    <div class="relative">
+      <input
+        type="text"
+        v-model="input"
+        placeholder="Enter text with kanji..."
+        class="w-96 bg-darkmode-100 border border-black rounded pl-7"
+      />
+      <i
+        class="fa-solid fa-magnifying-glass absolute top-1.5 left-1.5 text-black"
+      ></i>
+    </div>
+
     <AppButton
       :disabled="input === ''"
       :text="'Clear'"
