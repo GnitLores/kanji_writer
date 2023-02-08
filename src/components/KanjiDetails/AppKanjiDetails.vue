06e1e57c474ffd808b711f9db3e7e69c8c0c7bb0
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-02-08 18:18:02 +0100

    kanji details

--- src/components/KanjiDetails/AppKanjiDetails.vue
+++ src/components/KanjiDetails/AppKanjiDetails.vue
@@ -1,18 +1,53 @@
 <template>
-  <div class="container text-white text-opacity-80">
+  <div class="container">
     <div class="flex">
       <div class="flex-1 flex justify-center">
         <span class="mr-auto"
           ><div class="ml-2 mt-2">
-            <p class="text-4xl">
+            <p class="text-white text-4xl">
               {{ storeKanji.char }}
             </p>
             <ul class="capitalize font-semibold">
-              <li class="">- {{ storeKanji.kanjiData.keywords }}</li>
-              <li>Kunyomi: {{ storeKanji.kanjiData.kunyomi }}</li>
-              <li></li>
-            </ul></div
-        ></span>
+              <li class="pt-2 border-b border-gray-600">
+                <div class="text-white text-opacity-100">Keywords:</div>
+                <div class="text-white text-opacity-80">
+                  {{ storeKanji.kanjiData.keywords }}
+                </div>
+              </li>
+              <li class="pt-2 border-b border-gray-600">
+                <div class="text-white text-opacity-100">Kunyomi:</div>
+                <div class="text-white text-opacity-80">
+                  {{ storeKanji.kanjiData.kunyomi }}
+                </div>
+              </li>
+              <li class="pt-2 border-b border-gray-600">
+                <div class="text-white text-opacity-100">Onyomi:</div>
+                <div class="text-white text-opacity-80">
+                  {{ storeKanji.kanjiData.onyomi }}
+                </div>
+              </li>
+              <li class="pt-2 border-b border-gray-600">
+                <div class="text-white text-opacity-100">
+                  Class - {{ storeKanji.kanjiData.class }}
+                </div>
+                <div class="text-white text-opacity-100">
+                  Radical - {{ storeKanji.kanjiData.radical }}
+                </div>
+                <div class="text-white text-opacity-100">
+                  Kanken level - {{ storeKanji.kanjiData.kanken_level_text }}
+                </div>
+                <div class="text-white text-opacity-100">
+                  Jōyō -
+                  {{
+                    parseFloat(storeKanji.kanjiData.kanken_level) > 2
+                      ? "Yes"
+                      : "No"
+                  }}
+                </div>
+              </li>
+            </ul>
+          </div></span
+        >
       </div>
       <div class="flex-1 flex justify-center">
         <span><AppDetailsWriting /></span>
