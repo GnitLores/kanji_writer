c3dd6b00716e9f66e4d45e33d842699c8195ded2
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-02-08 09:59:22 +0100

    stroke order visualization

--- src/components/KanjiDetails/AppKanjiDetails.vue
+++ src/components/KanjiDetails/AppKanjiDetails.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="container">
+    <AppStrokeOrderVisualization />
     <AppDetailsWriting />
   </div>
 </template>
 
 <script setup>
 import AppDetailsWriting from "@/components/KanjiDetails/AppDetailsWriting.vue";
+import AppStrokeOrderVisualization from "@/components/KanjiDetails/AppStrokeOrderVisualization.vue";
 </script>
 
 <style scoped></style>
---
+++ src/components/KanjiDetails/AppStrokeOrderVisualization.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="flex">
+    <div class="flex flex-wrap p-2" ref="strokeOrderRef"></div>
+  </div>
+</template>
+
+<script setup>
+import {
+  ref,
+  reactive,
+  onMounted,
+  onBeforeUnmount,
+  onUnmounted,
+  computed,
+  watch,
+} from "vue";
+import { useStoreQuiz } from "@/stores/storeQuiz";
+import { useStoreKanji } from "@/stores/storeKanji";
+import { useStoreOptions } from "@/stores/storeOptions";
+
+import { useKanjiWriter } from "@/use/useKanjiWriter";
+
+const storeKanji = useStoreKanji();
+const storeQuiz = useStoreQuiz();
+const storeOptions = useStoreOptions();
+
+const KanjiWriter = useKanjiWriter();
+const strokeOrderRef = ref(null);
+
+// const boxSize = 50;
+
+const renderFanningStrokes = (target, strokes, nStrokes, strokeNr) => {
+  var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  const boxSize = nStrokes <= 10 ? 80 : nStrokes <= 20 ? 70 : 60;
+  svg.style.width = boxSize + "px";
+  svg.style.height = boxSize + "px";
+  svg.classList = "inline-block border border-gray-600 m-0.5";
+  target.appendChild(svg);
+  var group = document.createElementNS("http://www.w3.org/2000/svg", "g");
+
+  // set the transform property on the g element so the character renders at the boxSize
+  var transformData = KanjiWriter.getScalingTransform(boxSize, boxSize);
+  group.setAttributeNS(null, "transform", transformData.transform);
+  svg.appendChild(group);
+
+  strokes.forEach(function (strokePath) {
+    var path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttributeNS(null, "d", strokePath);
+    path.style.fill = "white";
+    path.style.fillOpacity = "80%";
+    group.appendChild(path);
+  });
+};
+
+const drawStrokeOrder = () => {
+  KanjiWriter.loadCharacterData(storeKanji.char).then(function (charData) {
+    var target = strokeOrderRef.value;
+    const nStrokes = charData.strokes.length;
+    for (var i = 0; i < nStrokes; i++) {
+      var strokesPortion = charData.strokes.slice(0, i + 1);
+      renderFanningStrokes(target, strokesPortion, nStrokes, i + 1);
+    }
+  });
+};
+
+onMounted(() => {
+  drawStrokeOrder();
+});
+</script>
+
+<style scoped></style>
