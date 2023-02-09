36c0e247aab6843f01f094630efbe83f6d179c72
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-02-09 16:21:28 +0100

    scaling zoom test

--- src/components/Modals/AppBaseModal.vue
+++ src/components/Modals/AppBaseModal.vue
@@ -1,14 +1,14 @@
 <template>
   <Teleport to="#modals">
     <!-- Show / hide the modal -->
-    <div v-if="show" class="">
+    <div v-if="show" class="mood">
       <!-- The backdrop -->
       <div class="fixed inset-0 bg-gray-900 opacity-40"></div>
 
       <!-- Where the actual content goes -->
       <div class="fixed inset-0 flex items-center justify-center">
         <div
-          class="bg-darkmode-700 rounded border-2 border-gray-600 w-full sm:w-[640px] md:w-[768px] lg:w-[1024px]"
+          class="bg-darkmode-700 rounded border-2 border-gray-600 w-full sm:w-[640px] md:w-[768px] lg:w-[1024px] xl:w-[1280px] 2xl:w-[1536px]"
         >
           <slot></slot>
         </div>
@@ -22,3 +22,16 @@ defineProps({
   show: Boolean,
 });
 </script>
+
+<style scoped>
+@media (-webkit-device-pixel-ratio: 1.25) {
+  .mood {
+    zoom: 0.8;
+  }
+}
+@media (-webkit-device-pixel-ratio: 1.5) {
+  .mood {
+    zoom: 0.66;
+  }
+}
+</style>
