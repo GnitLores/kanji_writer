fe4382a2f217ad24922c0aa5cb66f54fd46e2e35
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-02-09 16:29:32 +0100

    rename calss

--- src/components/Modals/AppBaseModal.vue
+++ src/components/Modals/AppBaseModal.vue
@@ -1,7 +1,7 @@
 <template>
   <Teleport to="#modals">
     <!-- Show / hide the modal -->
-    <div v-if="show" class="mood">
+    <div v-if="show" class="base-modal">
       <!-- The backdrop -->
       <div class="fixed inset-0 bg-gray-900 opacity-40"></div>
 
@@ -16,7 +16,6 @@
     </div>
   </Teleport>
 </template>
-<!-- class="bg-darkmode-700 rounded border-2 border-gray-600 w-full sm:w-[640px] md:w-[768px] lg:w-[1024px] xl:w-[1280px] 2xl:w-[1536px]" -->
 <script setup>
 defineProps({
   show: Boolean,
@@ -25,22 +24,22 @@ defineProps({
 
 <style scoped>
 @media (-webkit-device-pixel-ratio: 1.25) {
-  .mood {
+  .base-modal {
     zoom: 0.8;
   }
 }
 @media (-webkit-device-pixel-ratio: 1.5) {
-  .mood {
+  .base-modal {
     zoom: 0.66;
   }
 }
 @media (-webkit-device-pixel-ratio: 1.75) {
-  .mood {
+  .base-modal {
     zoom: 0.57;
   }
 }
 @media (-webkit-device-pixel-ratio: 2) {
-  .mood {
+  .base-modal {
     zoom: 0.5;
   }
 }
