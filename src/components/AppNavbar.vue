e628444500a0013535c141686a655c821811e700
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-02-10 10:14:12 +0100

    make navbar show current route

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -2,7 +2,8 @@
   <header class="select-none bg-darkmode-500 border-b border-gray-600">
     <nav class="container mx-auto flex justify-start items-center py-2">
       <router-link
-        class="text-white tracking-wider font-bold capitalize text-2xl mr-4 min-w-fit"
+        class="tracking-wider font-bold capitalize text-2xl mr-4 min-w-fit"
+        :class="[route.name === 'home' ? 'text-orange-400' : 'text-white']"
         :to="{ name: 'home' }"
         >Mojidoori <i class="fas fa-pen-to-square text-sky-400"></i
       ></router-link>
@@ -10,27 +11,52 @@
       <div class="flex flex-grow items-center">
         <ul class="flex flex-row mt-2 tracking-wide font-bold space-x-3">
           <li>
-            <router-link class="text-white" :to="{ name: 'learn' }"
+            <router-link
+              class=""
+              :class="[
+                route.name === 'learn' ? 'text-orange-400' : 'text-white',
+              ]"
+              :to="{ name: 'learn' }"
               >Learn</router-link
             >
           </li>
           <li>
-            <router-link class="text-white" :to="{ name: 'quiz' }"
+            <router-link
+              class=""
+              :class="[
+                route.name === 'quiz' ? 'text-orange-400' : 'text-white',
+              ]"
+              :to="{ name: 'quiz' }"
               >Quiz</router-link
             >
           </li>
           <li>
-            <router-link class="text-white" :to="{ name: 'kanji' }"
+            <router-link
+              class=""
+              :class="[
+                route.name === 'kanji' ? 'text-orange-400' : 'text-white',
+              ]"
+              :to="{ name: 'kanji' }"
               >Kanji</router-link
             >
           </li>
           <li>
-            <router-link class="text-white" :to="{ name: 'about' }"
+            <router-link
+              class=""
+              :class="[
+                route.name === 'about' ? 'text-orange-400' : 'text-white',
+              ]"
+              :to="{ name: 'about' }"
               >About</router-link
             >
           </li>
           <li>
-            <router-link class="text-white" :to="{ name: 'about' }"
+            <router-link
+              class=""
+              :class="[
+                route.name === 'login' ? 'text-orange-400' : 'text-white',
+              ]"
+              :to="{ name: 'about' }"
               >Login</router-link
             >
           </li>
@@ -40,6 +66,9 @@
   </header>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRoute, useRouter } from "vue-router";
+const route = useRoute();
+</script>
 
 <style scoped></style>
--- src/main.js
+++ src/main.js
@@ -11,4 +11,7 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 
+// router.isReady().then(() => {
+//   app.mount("#app");
+// });
 app.mount("#app");
