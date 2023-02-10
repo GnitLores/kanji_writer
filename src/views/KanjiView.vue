cedba45d2880cf77d6f3a94289ce7e93bcf56c32
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-02-10 12:28:48 +0100

    instruction when no kanji selected

--- src/views/KanjiView.vue
+++ src/views/KanjiView.vue
@@ -3,6 +3,11 @@
     <div v-if="storeKanji.char !== ''" class="flex justify-center">
       <AppKanjiDetails />
     </div>
+    <div v-else class="min-h-[600px] flex justify-center place-items-center">
+      <p class="text-white text-4xl font-semibold text-opacity-80">
+        Left any kanji to select
+      </p>
+    </div>
 
     <AppKanjiGrid selectionType="single" />
   </div>
