a85b88fad91d31745daab5ed7de43c0550e1d890
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-02-10 12:31:54 +0100

    min height to learn and quiz views

--- src/views/LearnView.vue
+++ src/views/LearnView.vue
@@ -1,4 +1,7 @@
 <template>
+  <div class="min-h-[600px] flex justify-center place-items-center">
+    <p class="text-white text-4xl font-semibold text-opacity-80">Learn</p>
+  </div>
   <div>
     <AppKanjiGrid />
   </div>
