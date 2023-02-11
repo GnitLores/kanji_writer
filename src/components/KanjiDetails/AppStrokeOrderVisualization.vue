a163bcb1c81f91c1b5d0e7d1de3b7ab1d72c4a45
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-02-11 11:57:52 +0100

    make stroke order visual more dynamic

--- src/components/KanjiDetails/AppStrokeOrderVisualization.vue
+++ src/components/KanjiDetails/AppStrokeOrderVisualization.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="slide">
-    <div v-show="storeOptions.showDetailsStrokeOrder">
+    <div v-show="storeOptions.showDetailsStrokeOrder" class="">
       <div class="flex">
         <div class="flex flex-wrap" ref="strokeOrderRef"></div>
       </div>
@@ -33,26 +33,76 @@ const { kanjiData } = storeToRefs(storeKanji);
 const KanjiWriter = useKanjiWriter();
 const strokeOrderRef = ref(null);
 
+const totalWidth = 300; // pixels
+const margin = 2; // pixels
+const borderWidth = 1; // pixels
+const perRowBig = 5; // boxes
+const perRowMed = 6; // boxes
+const perRowSmall = 7; // boxes
+const perRowExtraSmall = 8; // boxes
+const bigUpTo = 10; // boxes
+const medUpTo = 18; // boxes
+const smallUpTo = 21; // boxes
+let doSeparate = true;
+
 const emit = defineEmits(["strokeOrderClicked"]);
 
 const renderFanningStrokes = (target, strokes, nStrokes, strokeNr) => {
-  var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  const boxSize = nStrokes <= 10 ? 56 : nStrokes <= 18 ? 46 : 37;
-  svg.style.width = boxSize + "px";
-  svg.style.height = boxSize + "px";
-  svg.classList =
-    "inline-block border border-gray-600 bg-darkmode-700 hover:cursor-pointer hover:border-white";
-  if (nStrokes < 18) svg.classList += " m-0.5 rounded";
+  const boxSize = calcBoxSize(nStrokes);
+  const svg = createStrokeSvg(boxSize);
+  svg.appendChild(renderStrokes(boxSize, strokes));
+  target.appendChild(svg);
+
   svg.addEventListener("click", () => {
     emit("strokeOrderClicked", strokeNr, nStrokes);
   });
-  target.appendChild(svg);
-  var group = document.createElementNS("http://www.w3.org/2000/svg", "g");
+};
 
+const calcBoxSize = (nStrokes) => {
+  // The more strokes in the kanji, the more strokes per row which results in smaller boxes.
+  const nBoxesPerRow =
+    nStrokes <= bigUpTo
+      ? perRowBig
+      : nStrokes <= medUpTo
+      ? perRowMed
+      : nStrokes <= smallUpTo
+      ? perRowSmall
+      : perRowExtraSmall;
+
+  const totalMarginWidth = doSeparate ? nBoxesPerRow * margin * 2 : 0;
+  const totalBorderWidth = 2 * borderWidth * nBoxesPerRow;
+  const spaceForBoxes = totalWidth - totalMarginWidth - totalBorderWidth;
+
+  const boxSize = Math.floor(spaceForBoxes / nBoxesPerRow);
+
+  // console.log("NEW CALC");
+  // console.log("totalMarginWidth", totalMarginWidth);
+  // console.log("totalBorderWidth", totalBorderWidth);
+  // console.log("spaceForBoxes", spaceForBoxes);
+  // console.log("boxSize", boxSize);
+  // const effectiveWidth =
+  //   nBoxesPerRow * boxSize + nBoxesPerRow * 2 * (borderWidth + margin);
+  // console.log("effectiveWidth", effectiveWidth);
+
+  return boxSize;
+};
+
+const createStrokeSvg = (boxSize) => {
+  var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svg.style.width = boxSize + "px";
+  svg.style.height = boxSize + "px";
+  svg.classList =
+    "inline-block border-gray-600 bg-darkmode-700 hover:cursor-pointer hover:border-white";
+  if (doSeparate) svg.classList += ` m-[${margin}px] rounded`;
+  svg.classList += borderWidth === 1 ? " border" : ` border-${borderWidth}`;
+  return svg;
+};
+
+const renderStrokes = (boxSize, strokes) => {
   // set the transform property on the g element so the character renders at the boxSize
+  var group = document.createElementNS("http://www.w3.org/2000/svg", "g");
   var transformData = KanjiWriter.getScalingTransform(boxSize, boxSize);
   group.setAttributeNS(null, "transform", transformData.transform);
-  svg.appendChild(group);
 
   strokes.forEach(function (strokePath) {
     var path = document.createElementNS("http://www.w3.org/2000/svg", "path");
@@ -61,13 +111,15 @@ const renderFanningStrokes = (target, strokes, nStrokes, strokeNr) => {
     path.style.fillOpacity = "80%";
     group.appendChild(path);
   });
+  return group;
 };
 
 const drawStrokeOrder = () => {
   const storeKanji = useStoreKanji();
+  const strokes = storeKanji.writingData.strokes;
   const target = strokeOrderRef.value;
   for (var i = 0; i < storeKanji.nStrokes; i++) {
-    var strokesPortion = storeKanji.writingData.strokes.slice(0, i + 1);
+    var strokesPortion = strokes.slice(0, i + 1);
     renderFanningStrokes(target, strokesPortion, storeKanji.nStrokes, i + 1);
   }
 };
