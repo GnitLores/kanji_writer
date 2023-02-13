f03c13835620718478332a9f321aa515947b3cd3
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-02-13 11:00:29 +0100

    clean up

--- src/components/KanjiGrid/AppKanjiGridHeader.vue
+++ src/components/KanjiGrid/AppKanjiGridHeader.vue
@@ -1,32 +1,26 @@
 <template>
-  <div>
-    <div class="container mx-auto select-none my-2">
-      <AppLevelsBar />
-      <!--
-    ===============
-    Display Options:
-    ===============
-    -->
-      <transition name="slideDown">
-        <div v-show="storeOptions.showDisplayOptions">
-          <div class="flex">
-            <p class="text-sky-100 font-bold w-16">Display:</p>
-            <div class="inline-block ml-2">
-              <input
-                class=""
-                type="checkbox"
-                v-model="storeOptions.reverseOrder"
-              />
-              <label class="text-sky-100 font-bold ml-1">Reverse</label>
-            </div>
-            <div class="inline-block ml-4">
-              <input type="checkbox" v-model="storeOptions.doDisplayLevels" />
-              <label class="text-sky-100 font-bold ml-1">By level</label>
-            </div>
+  <div class="container mx-auto select-none my-2">
+    <AppLevelsBar />
+
+    <transition name="slideDown">
+      <div v-show="storeOptions.showDisplayOptions">
+        <div class="flex">
+          <p class="text-sky-100 font-bold w-16">Display:</p>
+          <div class="inline-block ml-2">
+            <input
+              class=""
+              type="checkbox"
+              v-model="storeOptions.reverseOrder"
+            />
+            <label class="text-sky-100 font-bold ml-1">Reverse</label>
+          </div>
+          <div class="inline-block ml-4">
+            <input type="checkbox" v-model="storeOptions.doDisplayLevels" />
+            <label class="text-sky-100 font-bold ml-1">By level</label>
           </div>
         </div>
-      </transition>
-    </div>
+      </div>
+    </transition>
   </div>
 </template>
 
