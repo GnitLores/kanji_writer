cef1026f554046c3ca07fd3ce4b54aab5e4f2c0e
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-02-14 13:39:28 +0100

    cache loaded kanji to reduce number of requests

--- src/use/useKanji.js
+++ src/use/useKanji.js
@@ -2,6 +2,9 @@ import { ref } from "vue";
 import { doc, getDoc } from "firebase/firestore";
 import { kanjiCollection } from "@/includes/firebase";
 
+const cachedKanji = new Map(); // Cache loaded data to reduce number of requests
+const maxCacheSize = 0; // max number of kanji to cache, 0 = no limit
+
 export function useKanji() {
   const char = ref("");
   const writingData = ref({});
@@ -16,16 +19,31 @@ export function useKanji() {
   };
 
   const loadKanji = async (charToLoad) => {
-    const docRef = doc(kanjiCollection, charToLoad);
-    const snapshot = await getDoc(docRef);
-    const loadedData = snapshot.data();
+    let loadedData = null;
+    if (cachedKanji.has(charToLoad)) {
+      loadedData = loadFromCache(charToLoad);
+    } else {
+      const docRef = doc(kanjiCollection, charToLoad);
+      const snapshot = await getDoc(docRef);
+      loadedData = snapshot.data();
+    }
 
     writingData.value = JSON.parse(loadedData.data);
     nStrokes.value = writingData.value.strokes.length;
     char.value = loadedData.kanji;
     data.value = loadedData;
 
-    console.log(loadedData);
+    saveToCache(loadedData);
+  };
+
+  const saveToCache = () => {
+    cachedKanji.set(char.value, data.value);
+    if (maxCacheSize > 0 && cachedKanji.size > maxCacheSize)
+      cachedKanji.delete(cachedKanji.keys().next().value); // Delete earliest entry
+  };
+
+  const loadFromCache = (charToLoad) => {
+    return cachedKanji.get(charToLoad);
   };
 
   return { char, writingData, nStrokes, data, init, loadKanji };
