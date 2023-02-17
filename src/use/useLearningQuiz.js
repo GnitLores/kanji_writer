10afc15f07f5c5951b7abf11b70141d0f5889140
GnitLores <prefuse@gmail.com>
2023-02-17 21:32:39 +0800

    create review batch

--- src/use/useLearningQuiz.js
+++ src/use/useLearningQuiz.js
@@ -21,9 +21,34 @@ export function useLearningQuiz() {
         if (selected.value[kanji.mainIdx]) kanjiToQuiz.value.push(kanji.char);
       });
     });
+    createReviewBatch();
   };
 
-  const createReviewBatch = () => {};
+  const createReviewBatch = () => {
+    const batchSize = storeOptions.learnBatchSize || kanjiToQuiz.value.length;
+    const kanjiBatch = [];
+    let i = 0;
+    while (i < batchSize && kanjiToQuiz.value.length > 0) {
+      kanjiBatch.push(kanjiToQuiz.value.shift());
+      i += 1;
+    }
+
+    if (storeOptions.learnShowLearningStep) addStep(kanjiBatch, "learn");
+    if (storeOptions.learnShowReinforceStep) addStep(kanjiBatch, "reinforce");
+    if (storeOptions.learnShowQuizStep) addStep(kanjiBatch, "quiz");
+  };
+
+  const addStep = (characters, stepType) => {
+    for (let i = 0; i < storeOptions.learnLearningStepRepetitions; i++) {
+      characters.forEach((char) => {
+        batchReviewQueue.value.push({
+          char,
+          stepType,
+          repetition: i + 1,
+        });
+      });
+    }
+  };
 
   return { initQuiz };
 }
